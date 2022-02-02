eefe0e6f72251bb9b107b3ac1b1ff780103808b7
CadetJ <68434464+Juniorxy@users.noreply.github.com>
2022-02-02 15:48:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#
+isalive = True
+name = "Calixte"
+age = 37
+height = 175.7 #the height is in centimeter#
+weight = 180 # weight is in pounds#
+eyes_color = "dark brown"
+fav_color = "Blue"
+denomination = "Christian"
+birth_month = "September"
+race = "chosen"
+ethnic = "royal"
+spoken_language = 2
+residing_state = "Florida"
+
+
+print(f"I am a {denomination} residing in {residing_state}. I am {age} years old. I am about {height} centimeters tall and weight about {weight} pounds. I have {eyes_color} eyes. To tell you a bit more about me, I'm from a {race} race and speak about {spoken_language} languages. Oh, yeah, I was born in the month of {birth_month} and my favorite color is {fav_color}.")
+
